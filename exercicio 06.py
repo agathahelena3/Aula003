e7a346c9cb720c5062dbf3f4b4e2703d987b2cf2
agathahelena3 <miauqsor@gmail.com>
2025-04-01 11:42:26 -0300

    alterei a atividade 06

--- exercicio 06.py
+++ exercicio 06.py
@@ -1,8 +1,8 @@
 from glob import glob1
 
 time1 = input ("Digite o nome do primeiro time: ")
-time2 = input ("Digite o nome do segundo time: ")
 goltime1 = int(input(f"Digite a quantidade de gols do {time1}."))
+time2 = input ("Digite o nome do segundo time: ")
 goltime2 = int(input(f"Digite a quantidade de gols do {time2}."))
 
 if goltime1 > goltime2:
