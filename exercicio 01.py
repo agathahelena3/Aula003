ab9048e53cc66e001635b389366de744179e5c9e
agathahelena3 <miauqsor@gmail.com>
2025-04-01 09:21:27 -0300

    atualização da atividade anterior

--- exercicio 01.py
+++ exercicio 01.py
@@ -1,5 +1,9 @@
 nome = input("Digite seu nome: ")
 idade = int(input("Digite sua idade: "))
 salário = float(input("Digite o seu salário:"))
+aumento = float(input("Digite a porcentagem do seu aumento:"))
+aumentoreal = salário * aumento /100
+salárioatual = salário + aumentoreal
 
-print (f"Olá, seu nome é {nome}, você tem {idade} anos e seu salário é {salário}.")
+
+print (f"Olá, seu nome é {nome}, você tem {idade} anos e seu salário é {salário:.2f}. O aumento do seu salário foi de {aumento}%, ou seja R${aumentoreal:.2f}. Seu salário atual é de R${salárioatual:.2f}")
