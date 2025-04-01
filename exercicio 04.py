295caf685ae37ab6796ad0ac275d1f9eb1a1a8ba
agathahelena3 <miauqsor@gmail.com>
2025-04-01 10:45:44 -0300

    modifiquei atividade 04

--- exercicio 04.py
+++ exercicio 04.py
@@ -1,8 +1,8 @@
 n1 = float(input("Digite o primeiro número:"))
 n2 = float(input("Digite o segundo número:"))
 if n1 < n2:
-    print (f"{n1}, {n2}")
-if n2 < n1:
-    print (f"{n2}, {n1}")
+    print (n1, n2)
+else:
+    print (n2,n1)
 
 
